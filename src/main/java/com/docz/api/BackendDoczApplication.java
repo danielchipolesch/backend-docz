c76bcdc02cf783555df2aedd2e28e5e1979ce059
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2023-01-17 23:41:11 -0300

    First commit

---
+++ src/main/java/com/docz/api/BackendDoczApplication.java
@@ -0,0 +1,13 @@
+package com.docz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDoczApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDoczApplication.class, args);
+	}
+
+}
